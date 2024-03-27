bc6c3dd8d2f458bb1f91489be6dc9ce7cc00e073
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-03-27 15:04:42 +0200

    initial commit

---
+++ week2/t1/t1.php
@@ -0,0 +1,40 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $color = $_POST['color'];
+    $size = $_POST['size'];
+    $styleString = '';
+
+    if (in_array('bold', $_POST['style']))
+        $styleString .= 'font-weight: bold;';
+    if (in_array('italic', $_POST['style']))
+        $styleString .= 'font-style: italic;';
+
+
+    echo '<p style="color: ' . $color . '; font-size: ' . $size . '; '. $styleString .'; font-weight: bold; font-style: italic;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
+}
+?>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <div>
+        <label for="red">Red</label>
+        <input type="radio" id="red" name="color" value="red">
+        <label for="green">Green</label>
+        <input type="radio" id="green" name="color" value="green">
+        <label for="blue">Blue</label>
+        <input type="radio" id="blue" name="color" value="blue">
+    </div>
+    <div>
+        <label for="size">Size:</label>
+        <select id="size" name="size">
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+        </select>
+    </div>
+    <div>
+        <label for="bold">Bold</label>
+        <input type="checkbox" id="bold" name="style" value="bold">
+        <label for="italic">Italic</label>
+        <input type="checkbox" id="italic" name="style" value="italic">
+    </div>
+    <button type="submit">Submit</button>
+</form>
